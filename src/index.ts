3f4369f4c2b0c3252f5b8dbe974245d788368177
David-282 <afolabidavid94@gmail.com>
2026-07-24 20:43:09 +0100

    fixing import issues

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { featureFlagsRouter } from './featureFlags';
+import { featureFlagsRouter } from './routes/featureFlags';
 import helmet from "helmet";
 import pinoHttp from "pino-http";
 import { v4 as uuidv4 } from "uuid";
